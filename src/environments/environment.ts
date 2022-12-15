ea453dff248d89a185ba9f2fdb5c39a418d52a56
Serhii Lemkin <jackflit51997@gmail.com>
2022-12-15 12:13:09 +0200

    123

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   title: 'Local Environment Heading',
   //apiURL: 'https://localhost:5001',
-  apiURL: 'https://realtimechartsserver20221213203537.azurewebsites.net',
+  apiURL: 'https://realtimechartsserver20221213203537.azurewebsites.net',  
 };
